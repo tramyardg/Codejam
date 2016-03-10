654b3eeae13dced4c82920a6f8d48b644dbacf5c
Raymart De Guzman <tramyardg@gmail.com>
2016-03-10 14:16:12 -0500

    ReverseWords.txt push.

--- src/codejam_class/ReverseWords.java
+++ src/codejam_class/ReverseWords.java
@@ -5,10 +5,40 @@
  */
 package codejam_class;
 
+//import java.util.Arrays;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+//import java.util.Scanner;
 /**
  *
  * @author RAYMARTHINKPAD
  */
 public class ReverseWords {
-    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter something: ");
+        String input = scan.nextLine();
+        System.out.println(reverseIt(storeStrArry(input)));
+    }
+
+    public static String[] storeStrArry(String words) {
+        String[] str = words.split(" ");
+        return str;
+    }
+
+    public static String reverseIt(String[] str) {
+        List<String> list = Arrays.asList(str);
+        Collections.reverse(list);
+
+        str = (String[]) list.toArray();
+
+        String joined = String.join(" ", str);
+        return joined;
+    }
+
 }
