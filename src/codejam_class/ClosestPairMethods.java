3ed6198b641c5b1bbd06bbb3db39cd328725cec4
Raymart De Guzman <tramyardg@gmail.com>
2016-03-14 22:46:02 -0400

    New challenge from CodeEval coding - Closest Pair!

---
+++ src/codejam_class/ClosestPairMethods.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codejam_class;
+
+/**
+ *
+ * @author RAYMARTHINKPAD
+ */
+public class ClosestPairMethods {
+    
+}
